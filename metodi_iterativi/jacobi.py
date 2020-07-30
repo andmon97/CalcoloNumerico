37a8745225b5436fbae59f196591aeac95f863cc
Andrea Montemurro <andrea.montemurro97@gmail.com>
2020-07-30 23:48:38 +0200

    jacobi fix

--- metodi_iterativi/jacobi.py
+++ metodi_iterativi/jacobi.py
@@ -21,11 +21,11 @@ def Jacobi(A,b,x0,tol,Kmax,xsol):
     while not (stop) and k < Kmax:
         for i in range(n):
             S = 0
-        for j in range(0, i):
-            S = S + A[i, j] * x0[j]
-        for j in range(i + 1, n):
-            S = S + A[i, j] * x0[j]
-        x1[i] = (b[i] - S) / A[i, i]
+            for j in range(0, i):
+                S = S + A[i, j] * x0[j]
+            for j in range(i + 1, n):
+                S = S + A[i, j] * x0[j]
+            x1[i] = (b[i] - S) / A[i, i]
 
         # Controllo della convergenza
         res_rel = la.norm(b - np.dot(A, x1)) / la.norm(b)  # residuo
@@ -50,28 +50,30 @@ def Jacobi(A,b,x0,tol,Kmax,xsol):
 
     return x1, k, err_rel_vett, residuo, diff_it_succ
 
-    # Costurzione del problema test con matrice a predominanza diagonale
-    n = 1200;
-    c = 10;  # se c è grande la convergenza è piu veloce
-    e1 = np.ones(n - 1)
-    A = np.diag(e1, -1) - c * np.eye(n) + np.diag(e1, 1)
-    xsol = np.ones(n)
-    b = np.dot(A, xsol)
 
-    # Parametri per il metodo: vettore di soluzione random con valori tra 1 e 10
-    x0 = np.array([float(math.ceil(random() * 10)) for i in range(n)])
-    Kmax = 100;
-    tol = 1.0e-6;
+# Costurzione del problema test con matrice a predominanza diagonale
+n = 1200;
+c = 10;  # se c è grande la convergenza è piu veloce
+e1 = np.ones(n - 1)
+A = np.diag(e1, -1) - c * np.eye(n) + np.diag(e1, 1)
+xsol = np.ones(n)
+b = np.dot(A, xsol)
 
-    x, k, err_rel_vett, residuo, diff_it_succ = Jacobi(A, b, x0, tol, Kmax, xsol)
 
-    import matplotlib.pyplot as plt
-    plt.figure(1)
+# Parametri per il metodo: vettore di soluzione random con valori tra 1 e 10
+x0 = np.array([float(math.ceil(random() * 10)) for i in range(n)])
+Kmax = 100;
+tol = 1.0e-6;
 
-    plt.semilogy(range(k), err_rel_vett[0:k], label='Errore effettivo')
-    plt.semilogy(range(k), residuo[0:k], label='Residuo')
-    plt.semilogy(range(k), diff_it_succ[0:k], label='Diff. Iter. Succ.')
-    plt.semilogy(range(k), np.ones(k) * tol, '--', label='Tolleranza')
-    plt.legend()
-    plt.xlabel('k')
-    plt.ylabel('Errore')
+x, k, err_rel_vett, residuo, diff_it_succ = Jacobi(A, b, x0, tol, Kmax, xsol)
+
+import matplotlib.pyplot as plt
+plt.figure(1)
+
+plt.semilogy(range(k), err_rel_vett[0:k], label='Errore effettivo')
+plt.semilogy(range(k), residuo[0:k], label='Residuo')
+plt.semilogy(range(k), diff_it_succ[0:k], label='Diff. Iter. Succ.')
+plt.semilogy(range(k), np.ones(k) * tol, '--', label='Tolleranza')
+plt.legend()
+plt.xlabel('k')
+plt.ylabel('Errore')
